2c95682266befc509a4fd9f26331a206ce55a5db
Giorgi Adeishvili <adeishvili65@gmail.com>
2022-02-02 01:30:09 +0400

    initial xcode project creation

---
+++ youtube-onedaybuild-tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild-tutorial
+//
+//  Created by Giorgi Adeishvili on 02.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
