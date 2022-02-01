040a523622f37aeba76eab5dcc0c46771cda2b24
Giorgi Adeishvili <adeishvili65@gmail.com>
2022-02-02 03:20:46 +0400

    add code to parse json resopnse from api

---
+++ youtube-onedaybuild-tutorial/Response.swift
@@ -0,0 +1,29 @@
+//
+//  Response.swift
+//  youtube-onedaybuild-tutorial
+//
+//  Created by Giorgi Adeishvili on 02.02.22.
+//
+
+import Foundation
+
+struct Response: Decodable {
+    
+    var items: [Video]?
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case items
+        
+    }
+    
+    init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.items = try container.decode([Video].self, forKey: .items)
+        
+    }
+    
+    
+}
