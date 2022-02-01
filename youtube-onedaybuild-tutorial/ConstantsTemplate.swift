48201bae97c22e58cd8822094331b6a8d540d25d
Giorgi Adeishvili <adeishvili65@gmail.com>
2022-02-02 03:30:45 +0400

    modified git ignore, add constantsTemplate

--- youtube-onedaybuild-tutorial/ConstantsTemplate.swift
+++ youtube-onedaybuild-tutorial/ConstantsTemplate.swift
@@ -1,5 +1,5 @@
 //
-//  Constants.swift
+//  ConstantsTemplate.swift
 //  youtube-onedaybuild-tutorial
 //
 //  Created by Giorgi Adeishvili on 02.02.22.
@@ -7,8 +7,10 @@
 
 import Foundation
 
+/*
 struct Constrants {
-    static var API_KEY = "[API KEY]"
-    static var PLAYLIST_ID = "UUxOLe5mYu268htjkSOSxqEQ"
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
     static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constrants.PLAYLIST_ID)&key=\(Constrants.API_KEY)"
 }
+ */
